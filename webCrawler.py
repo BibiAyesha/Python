0ac5fbfe1abc820a7d6a4e89d527fd2c43694796
BibiAyesha <BibiAyesha@users.noreply.github.com>
2016-11-22 16:46:47 +0530

    Update webCrawler.py

--- webCrawler.py
+++ webCrawler.py
@@ -1,9 +1,30 @@
+#webCrawler.py
+
+"""
+    In this python script I have used
+    1. urllib to connect to the server and get the html page
+    2. BeautifulSoap to get and to walk through the DOM structure
+    the HTML page
+    3. sys package to get the sommand line arguments
+
+"""
 import sys
 import urllib.request, bs4
 
+
+"""
+In the function getRespKeyWord
+   1. fetching the keyword, and creating the url
+   2. sending it to the server and getting html page of the URL.
+   3. creating beatifulsoap class from the html that we got from the above step.
+   4. fetching the corresponding div to get the results
+   (total number of results are desplayed in the div which has class numTotalResults, so fetching the class from beautiful structure.
+    the result is displayed in the specified format
+    example - Results 161 - 200 of 1128
+    splitting the above results and getting the last num which represents total number of results for the given query.)
+"""
 def getRespKeyword(keyWord):
     url = "http://www.shopping.com/products"+"?KW="+keyWord
-    print(url)
     res = urllib.request.Request(url)
     response = urllib.request.urlopen(res)
     soup = bs4.BeautifulSoup(response.read(),"html.parser")
@@ -16,8 +37,16 @@ def getRespKeyword(keyWord):
         print("total number of products for the keyword ",keyWord," is : ",num)
     return num
 
-
-
+""""
+In the Function getRespPgNum 
+  1. fetch the pgNum and keyword to form the url
+  2. check if pgNum is int, otherwise return -1
+  3. If the pgNum is 0, then it doesn't exist, so return -1
+  4. In the DOM structure find the form which holds all the results on the page. 
+  5. select all the divs under the form then count the div using beautifulsoap.
+  6. The divs under the form are the displayed items.
+  7. If there are no divs then there are no items, so return -1.
+"""
 
 def getRespPgNum(keyWord, pgNum):
     if isinstance(pgNum, int):
